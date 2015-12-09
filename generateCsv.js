8e277d339f8c6cc20b4efc8231b4b71da54a45be
Justin Tanacredi <jtanacredi@gmail.com>
2015-12-08 23:58:04 -0500

    Regex - modify regex check for repeated tries. Add aditional code for when checks end.

--- generateCsv.js
+++ generateCsv.js
@@ -1,18 +1,28 @@
 fs = require('fs');
 
 // var file = "/Users/jtanacredi/Dev/testing/translation-parser/index.phtml"
-var file = "/Users/justintanacredi/Dev/node/translation-csv-generator/index.phtml"
+var file = "/Users/justintanacredi/Dev/node/translation-csv-generator/index.phtml",
+    regex = /(->__\()(.*)(\);)/g,
+    matches = [],
+    match;
+
+
 
 fs.readFile(file, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
   // regex for everything between ->__( and );
-  var regex = /(->__\()(.*)(\);)/;
 
-
-  var result = regex.exec(data);
-  console.log(result);
+  while ((match = regex.exec(data)) != null) {
+      matches.push(match[1]);
+      match = regex.exec(data);
+      console.log(matches);
+  }
+  // console.log(matches);
+  //
+  // var result = regex.exec(data);
+  // console.log(result);
 
 
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
