cb015cb92bc8b392c560834d43a73978502e9463
Justin Tanacredi <jtanacredi@gmail.com>
2015-12-08 20:52:55 -0500

    Regex - update regex to pick up string within translate function

--- generateCsv.js
+++ generateCsv.js
@@ -1,17 +1,20 @@
 fs = require('fs');
 
-var file = "/Users/jtanacredi/Dev/testing/translation-parser/index.phtml"
+// var file = "/Users/jtanacredi/Dev/testing/translation-parser/index.phtml"
+var file = "/Users/justintanacredi/Dev/node/translation-csv-generator/index.phtml"
 
 fs.readFile(file, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  // console.log(data);
-  // var regex = /[\-\>]+/;
-  var regex = /(->__\()/;
+  // regex for everything between ->__( and );
+  var regex = /(->__\()(.*)(\);)/;
+
+
   var result = regex.exec(data);
   console.log(result);
-  //  TAKE result and continue parsing for strings to translate. Move to csv
+
+
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
-  console.log(result[0]);
+  // console.log(result[0]);
 });
